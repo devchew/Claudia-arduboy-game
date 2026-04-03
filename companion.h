f9560072def3b8a581e11f94ede06d950182a42a
maciej.ptasinski <maciej.ptasinski@pracuj.pl>
2026-04-03 15:35:30 +0200

    progmem for companion

--- companion.h
+++ companion.h
@@ -15,23 +15,41 @@ uint8_t companionPopupCloseTimeout = 0;
 
 uint8_t introSequence = 0;
 
-const String helpPrompts[13] = {
-  /* 0  */ "Initializing system...",
-  /* 1  */ "Connecting to node...",
-  /* 2  */ "Data center status:\nONLINE",
-  /* 3  */ "Assigned role:\nOperator",
-  /* 4  */ "Booting assistant...",
-  /* 5  */ "Learning...",
-  /* 6  */ "Hello.\nI am Cloud'ia.",
-  /* 7  */ "I will assist you\nin managing\nthis infrastructure.",
-  /* 8  */ "Current load:\nLOW",
-  /* 9  */ "All systems stable.",
-  /* 10 */ "You may begin.",
-  /* 11 */ "System is overloaded\nbuy more servers\nor users will leave",
-  /* 12 */ "System is overloaded\nusers are leaving!"
+const PROGMEM char helpPrompt0[] = "Initializing system...";
+const PROGMEM char helpPrompt1[] = "Connecting to node...";
+const PROGMEM char helpPrompt2[] = "Data center status:\nONLINE";
+const PROGMEM char helpPrompt3[] = "Assigned role:\nOperator";
+const PROGMEM char helpPrompt4[] = "Booting assistant...";
+const PROGMEM char helpPrompt5[] = "Learning...";
+const PROGMEM char helpPrompt6[] = "Hello.\nI am Cloud'ia.";
+const PROGMEM char helpPrompt7[] = "I will assist you\nin managing\nthis infrastructure.";
+const PROGMEM char helpPrompt8[] = "Current load:\nLOW";
+const PROGMEM char helpPrompt9[] = "All systems stable.";
+const PROGMEM char helpPrompt10[] = "You may begin.";
+const PROGMEM char helpPrompt11[] = "System is overloaded\nbuy more servers\nor users will leave";
+const PROGMEM char helpPrompt12[] = "System is overloaded\nusers are leaving!";
+const PROGMEM char helpPrompt13[] = "Great progress!\nUsers are flowing in.\nSystems are stable.";
+const PROGMEM char helpPrompt14[] = "Users are flooding in!\nWe need more servers\nto handle the load.";
+
+const char* const helpPrompts[15] PROGMEM = {
+  helpPrompt0,
+  helpPrompt1,
+  helpPrompt2,
+  helpPrompt3,
+  helpPrompt4,
+  helpPrompt5,
+  helpPrompt6,
+  helpPrompt7,
+  helpPrompt8,
+  helpPrompt9,
+  helpPrompt10,
+  helpPrompt11,
+  helpPrompt12,
+  helpPrompt13,
+  helpPrompt14
 };
 
-boolean helpProptsState[13] = {
+boolean helpProptsState[15] = {
   false
 };
 
@@ -56,7 +74,10 @@ boolean drawCompainionHelp(uint8_t helpIndex) {
     arduboy.drawLine(12, 52, min(companionPopupCloseTimeout, 90) + 20, 52, WHITE);
   }
   font3x5.setCursor(11, 8);
-  font3x5.print(helpPrompts[helpIndex]);
+
+  char buffer[64];
+  strcpy_P(buffer, (char*)pgm_read_word(&(helpPrompts[helpIndex])));
+  font3x5.print(buffer);
 
   arduboy.fillRect(0, 56,128,8, BLACK);
   Arduboy2Base::drawBitmap(90, 57, buttonsRight, 7, 8, WHITE);
@@ -90,6 +111,15 @@ void compainionHelp() {
         introSequence = 255;
         currentScreen = 1; // 0 - server; 1 - office; 2 - settings
       }
+    }
+  }
+
+  // milestone: 300 inbound - message depends on load
+  if (inbound >= 300 && !helpProptsState[13] && !helpProptsState[14]) {
+    if (loadPercent < 80) {
+      drawCompainionHelp(13);
+    } else {
+      drawCompainionHelp(14);
     }
   }
 
