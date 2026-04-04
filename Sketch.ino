88813180cf31a1e6794fa15f47d435a13b49507d
maciej.ptasinski <maciej.ptasinski@pracuj.pl>
2026-04-04 12:16:39 +0200

    todo

--- Sketch.ino
+++ Sketch.ino
@@ -19,18 +19,10 @@
 
 todo:
 
-- [x] pierwsza seria upgrade nie pozwala na zapełnienie pierwszego racka, trzeba podbić bonusy
-wszystkie bonusy dają max 406 userów, wymaxowany serwer to 1.2k
-upgrade office powinna kończyć się na 150% capacity serwera userów, aby przekroczyć capacity serwera
-- [x] komunikat: 300 U/m - claudia: informacja, ze dobrze idzie, widać napływ userów, i w zalenosci od loadu komuniakt o stabilności systemu, albo o potrzebie dokupienia serwerów
-- [x] gdy wykupimy minimum łącznie 60lvl serwera  - komunikat od claudi ze mamy juz dużo serwerów i trzeba zainwestować w kolejna serwerownie
-- [x] koszt nowego racka: 100K, max 3 racki
-- [x] overload od 110% - komunikat o overloadzie i potrzebie dokupienia serwerów
-- [x] overload od 150% - komunikat o krytycznym overloadzie i odpływie userów
-- [x] po wykupieniu 2go serwera odblokowują się kolejne upgrade do nr 7, komunikat o nowych upgrade
-- [x] claudia zaczyna byc self aware po pierwszych ai update
-- [x] po wykupieniu 3ch serwerów i minimum 3/4 wypełnienia każdego odblokowuje się ostatnia seria upgrade
-- [ ] po zakupieniu ostatniego upgrade claudia wyswietla ostatni komunikat i konic gry
+- [ ] ostatnia porcja komunikatw claudii, wyswietla sie tylko pierwszy i od razu pojawia sie ekran końćowey.
+brakuje pozstałych komunikatów, a powinny sie pojawiać po kolei co x sekund, a po ostatnim pojawiać ekran końcowy.
+- [ ] ostatni upgrade powinien kosztować 1mln, a nie 100k
+- [ ] poprzednie dwa upgrade poiwnny być droższe, żeby gracz nie kupował ich od razu po odblokowaniu
 
 */
 
